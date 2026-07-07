969f0986f8fc3e2260e01a80e623128817516e62
Akash-x1i <akashatcode@gmail.com>
2026-07-07 16:18:02 +0530

    array and objects

---
+++ JS/array.js
@@ -0,0 +1,25 @@
+const arr = [10,20,30,40,50,60,70,80,90]
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+arr.push(90);
+console.log(arr);
+
+
+//converting into array:
+
+const num = 12367890;
+console.log(num.split())
+
+const alpha = "efghijklmnopqrst"
+console.log(alpha.split(""))   // splitting each letters
+
+const name = "akash bauri"
+console.log(name.split(" "))   // splitting each words
+
+
+
+// slice:
+
+console.log(arr.slice(2,15))
---
+++ JS/objects.js
@@ -0,0 +1,10 @@
+const marker = {
+    name : "camlin",
+    color : "yellow",
+    mfg : "Ludhiana"
+}
+marker.color = "red";
+marker.price = 50;
+delete(marker.mfg);
+console.log(marker);
+
