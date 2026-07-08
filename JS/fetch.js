19dc59a4aa51aaaf687f146cccef9efd2ef2f9b3
Akash-x1i <akashatcode@gmail.com>
2026-07-08 16:19:44 +0530

    fetching

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,5 +1,14 @@
-array = [1, 2, 3, 4, 5]
+// array = [1, 2, 3, 4, 5]
 
-array.map((element)=>{
-    console.log(element)
-})
+// array.map((element)=>{
+//     console.log(element)
+// })
+
+
+const data = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())        //parsing to json
+.then((data)=>                  //converting to array
+    data.map((user)=>{          //iterating over each object inside the array 
+    console.log("User Details: ", user.name);
+  })
+)
